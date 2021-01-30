4cc913b7ad270b880e4b020d10f3b95116b72aac
Patrick Dodd <kasperkain@gmail.com>
2021-01-29 19:48:42 -0500

    slight modifications

--- src/server.js
+++ src/server.js
@@ -2,9 +2,17 @@ const db = require('./db');
 const app = require('./app');
 const { PORT } = require('./config');
 
-const { connectionString } = require('../postgrator-config');
-
 app.set('db', db);
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { error };
+    res.status(500).json(response);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
