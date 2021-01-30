852bc4833b710ec06ccbc9872fe8404d3b552509
Patrick Dodd <kasperkain@gmail.com>
2021-01-29 20:00:26 -0500

    altered migrations

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,5 +1,5 @@
 module.exports = {
   migrationsDirectory: 'migrations',
   driver: 'pg',
-  connectionString: 'postgres://postgres@localhost/poem_db',
+  connectionString: process.env.DATABASE_URL,
 };
